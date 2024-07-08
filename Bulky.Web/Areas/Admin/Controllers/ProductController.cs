e4fec71dfa44aa844bd86ef0834179498792b3c6
ojaswinik <ojaswinir1@gmail.com>
2024-07-08 16:40:12 +1000

    ViewBag, ViewData concept ViewModel concept, Upsert(Combining Create and Edit pages in one Upsert)

--- Bulky.Web/Areas/Admin/Controllers/ProductController.cs
+++ Bulky.Web/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -12,71 +13,73 @@ namespace BulkyBook.Web.Areas.Admin.Controllers
 
         public ProductController(IUnitOfWork unitOfWork)
         {
-                _unitOfWork = unitOfWork;
+            _unitOfWork = unitOfWork;
         }
 
 
         public IActionResult Index()
         {
             List<Product> products = _unitOfWork.Product.GetAll().ToList();
+
+            return View(products);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            //Here we are using All Categories list in Dropdown for every product
             IEnumerable<SelectListItem> categories = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
             {
                 Text = u.Name,
                 Value = u.CategoryId.ToString()
             });
-            return View(products);
-        }
 
-        public IActionResult Create()
-        {
-            return View();
+            //Ways to pass this list to Product view
+            //1. ViewBag
+            //ViewBag.CategoryList = categories;
+            //2. ViewData
+            //ViewData["CategoryList"] = categories;
+            //3. Using new ViewModel like ProductViewModel
+
+            ProductViewModel productViewModel = new ProductViewModel()
+            {
+                Product = new Product(),
+                CategoryList = categories
+            };
+
+            if (id == null || id == 0)
+            {
+                //Create
+                return View(productViewModel);
+            }
+            else
+            {
+                //Update
+                productViewModel.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productViewModel);
+            }
+
         }
 
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Upsert(ProductViewModel productVM, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
-                _unitOfWork.Save();                
+                _unitOfWork.Product.Add(productVM.Product);
+                _unitOfWork.Save();
                 //We can pass Tempdata message to next rendered view. Once page is refreshed TempData value goes away. TempData is created only to pass on some messages to next Rendered view.
                 TempData["success"] = "Product created sucessfully.";
                 return RedirectToAction("Index");
             }
-
-            return View();
-        }
-
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
+            else
             {
-                return NotFound();
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.CategoryId.ToString()
+                });
+                return View(productVM);
             }
-            Product? productRow = _unitOfWork.Product.Get(u => u.Id == id);            
-
-            if (productRow == null)
-            {
-                return NotFound();
-            }
-            return View(productRow);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-
-            //ModelState.IsValid is for Server side validation which MaxLength and range
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();                
-                TempData["success"] = "Product updated sucessfully.";
-                return RedirectToAction("Index");
-            }
-
-            return View();
-
         }
 
         public IActionResult Delete(int? id)
@@ -85,7 +88,7 @@ namespace BulkyBook.Web.Areas.Admin.Controllers
             {
                 return NotFound();
             }
-            Product? productRow = _unitOfWork.Product.Get(u => u.Id == id);            
+            Product? productRow = _unitOfWork.Product.Get(u => u.Id == id);
 
             if (productRow == null)
             {
